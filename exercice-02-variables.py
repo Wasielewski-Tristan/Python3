cc4df028f3d46017a25f99e2e5d16f10b20bf4fa
Tristan Wasielewski <tristan.wasielewski@outlook.fr>
2022-07-07 16:09:14 +0200

    Exo fini de Phyton
    Exo terminer de 1 a 12.

---
+++ exercice-02-variables.py
@@ -0,0 +1,69 @@
+# exercice-02-variables.py
+
+# exo 2.1
+# Affectez :
+# - le nombre 42 à une variable
+# - le nombre d'or 1,61 à une variable
+# - votre nom et prénom à une variable
+# - la valeur booléenne vrai si nous sommes le matin, sinon la valeur booléenne faux, à une variable
+# - la valeur null `None` à une variable
+# Affichez ces variables
+
+# réponse 2.1
+nombre = 42
+print(nombre)
+
+nombre = 1.61
+print(nombre)
+
+name = "Tristan Wasielewski"
+print(name)
+
+matin = True
+print(matin)
+
+midi = False
+print(midi)
+
+nul = None
+print(nul)
+
+# code 2.1
+# la fonction `round()` permet d'arrondir un float en un integer
+# 0,1 est arrondi à la valeur inférieur
+print(round(0.1))
+# 0,9 est arrondi à la valeur supérieur
+print(round(0.9))
+# la fonction `round()` permet aussi d'arrondir en précisant le nombre de chiffres après la virgule
+# arrondir à un nombre décimal à 4 chiffres après la virgule
+print(round(1 / 3, 4))
+
+# exo 2.2
+# Stockez le valeurs suivantes dans une variable et transtypez-les :
+# - integer 2 en un float
+# - float 1,62 en un integer
+# - float 1,62 en un float arrondi à zéro chiffre après la virgule, puis en un integer
+# - float 1,62 en un float arrondi à un chiffre après la virgule
+# À chaque fois stockez le résultat dans une variable et affichez le résultat.
+
+# réponse 2.2
+
+nombre = 2 
+nombre = float(nombre)
+print(nombre)
+
+nombre = 1.62
+nombre = int(nombre)
+print(nombre)
+
+nombre = 1.62
+nombre = round(nombre, 0)
+print(nombre)
+nombre = int(nombre)
+print(nombre)
+
+nombre = 1.62
+nombre = round(nombre, 1)
+print(nombre)
+
+
